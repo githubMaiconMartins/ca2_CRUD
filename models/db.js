1bb20912f3d36744bc8fbe02a44f786a217d343c
githubMaiconMartins <githubmaiconmartins@gmail.com>
2022-05-06 09:31:34 +0000

    db connection modified

--- models/db.js
+++ models/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 //connection to my database
+/* Copy the line of code from file Readme here */
 
 
-
-dbconnect = mongoose.connect('mongodb+srv://admin:admin123@cluster0.bvukp.mongodb.net/BucketListDB', { useNewUrlParser: true }, (err) => {
+dbconnect = mongoose.connect('/* Copy the line of code from Read me file  here */', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('-------------------------MongoDB Connection Was a Success.----------------------------------') }
     else { console.log('Error in DB connection : ' + err) }
 });
